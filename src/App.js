79826f0bfde6f165d3f6cdf52e11281ca97f2e9b
akshay2903 <akshaypal2903@gmail.com>
2023-04-23 22:48:48 +0530

    Logout User

--- src/App.js
+++ src/App.js
@@ -10,42 +10,44 @@ import "./App.css";
 
 function App() {
 
-  const [displayName, setDisplayName] = useState('');
-  const [photoUrl, setPhotoUrl] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
 
-  useEffect(() => {
-    fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDTHcN6BfHG9yJUF7SSWSe8c7ZWnwhUxOQ',{
-      method:'POST',
-      body:JSON.stringify({
-        idToken:localStorage.getItem('idToken')
-      }),
-      headers:{
-        'Content-Type':'applicatio/json'
-      }
-    }).then((res) => {
-      if(res.ok){
-        return res.json();
-      }else{
-        return res.json((data) => {
-          throw new Error(data.error.message)
-        })
-      }
-    }).then((data) => {
-      setDisplayName(data.displayName);
-      setPhotoUrl(data.photoUrl)
-    }).catch((error) => {
-      alert(error);
-    })
-  },[])
+  // const [displayName, setDisplayName] = useState('');
+  // const [photoUrl, setPhotoUrl] = useState('');
+
+  // useEffect(() => {
+  //   fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDTHcN6BfHG9yJUF7SSWSe8c7ZWnwhUxOQ',{
+  //     method:'POST',
+  //     body:JSON.stringify({
+  //       idToken:localStorage.getItem('idToken')
+  //     }),
+  //     headers:{
+  //       'Content-Type':'applicatio/json'
+  //     }
+  //   }).then((res) => {
+  //     if(res.ok){
+  //       return res.json();
+  //     }else{
+  //       return res.json((data) => {
+  //         throw new Error(data.error.message)
+  //       })
+  //     }
+  //   }).then((data) => {
+  //     setDisplayName(data.displayName);
+  //     setPhotoUrl(data.photoUrl)
+  //   }).catch((error) => {
+  //     alert(error);
+  //   })
+  // },[])
 
   return (
     <>
-      <Header/>
+      <Header login ={isLogin} setLogin = {setIsLogin}/>
       <Routes>
         <Route exact path="/" element = {<SignUp/>}/>
         <Route exact path="/welcome" element = {<Welcome/>}/>
-        <Route exact path="/login" element = {<Login/>}/>
-        <Route exact path="/completeprofile" element={<Profile inputName = {displayName} inputUrl = {photoUrl} />} />
+        <Route exact path="/login" element = {<Login setLogin = {setIsLogin}/>}/>
+        <Route exact path="/completeprofile" element={<Profile/>} />
       </Routes>
     </>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,88 +1,86 @@
-import React, { useRef } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useRef } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import "./Login.css";
 
+const Login = (props) => {
+  const loginEmailRef = useRef();
+  const loginPasswordRef = useRef();
+  const navigate = useNavigate();
 
-const Login = () => {
+  const loginSubmitHandler = (event) => {
+    event.preventDefault();
 
-    const loginEmailRef = useRef();
-    const loginPasswordRef = useRef();
-    const navigate = useNavigate();
+    const enteredloginEmail = loginEmailRef.current.value;
+    const enteredloginPassword = loginPasswordRef.current.value;
 
-    const loginSubmitHandler = (event) => {
-        event.preventDefault();
-
-        const enteredloginEmail = loginEmailRef.current.value;
-        const enteredloginPassword = loginPasswordRef.current.value;
-
-        fetch(
-            "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDTHcN6BfHG9yJUF7SSWSe8c7ZWnwhUxOQ",
-            {
-              method: "POST",
-              body: JSON.stringify({
-                email: enteredloginEmail,
-                password: enteredloginPassword,
-                returnSecureToken: true,
-              }),
-            }
-          )
-          .then((res) => {
-            if(res.ok){
-                alert("Login success");
-                console.log("Login success", res);
-                return res.json();
-            } else{
-                return res.json().then((data) => {
-                    alert(data.error.message);
-                    throw new Error(data.error.message);
-                })
-            }
-          })
-          .then((data) => {
-            console.log(data);
-            navigate("/welcome");
-            // localStorage.setItem("TokenID Expense", data.idToken);
-            console.log("data", data);
-          })
-          .catch((error) =>{
-            alert("Something went wrong")
-            console.log("Something went wrong");
-          })
-
-    };
-    return (
-        <div className="loginBody">
-          <form onSubmit={loginSubmitHandler} className="form">
-          <h1>Login</h1>
-            <div>
-              <input
-                type="email"
-                placeholder="Email"
-                required
-                ref={loginEmailRef}
-                autoComplete='on'
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                placeholder="Password"
-                required
-                ref={loginPasswordRef}
-                autoComplete='on'
-              />
-            </div>
-            <div>
-              <button className="LoginBtn">Login</button>
-            </div>
-            <span className="tosignup">
-              <p>
-                Don't have Account? <Link to="/login">Sign Up</Link>
-              </p>
-            </span>
-          </form>
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDTHcN6BfHG9yJUF7SSWSe8c7ZWnwhUxOQ",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredloginEmail,
+          password: enteredloginPassword,
+          returnSecureToken: true,
+        }),
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          alert("Login Success");
+          console.log("Login Success", res);
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            alert(data.error.message);
+            throw new Error(data.error.message);
+          });
+        }
+      })
+      .then((data) => {
+        localStorage.setItem("idToken", data.idToken);
+        props.setLogin(true);
+        navigate("/welcome");
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+        console.log("Something went wrong");
+      });
+  };
+  return (
+    <div className="loginBody">
+      <form onSubmit={loginSubmitHandler} className="form">
+        <h4>Login</h4>
+        <div>
+          <input
+            type="email"
+            id="loginEmail"
+            placeholder="Email-Id"
+            required
+            autoComplete="on"
+            ref={loginEmailRef}
+          />
         </div>
-      );
-    };
+        <div>
+          <input
+            type="password"
+            id="loginPassword"
+            placeholder="Password"
+            required
+            autoComplete="on"
+            ref={loginPasswordRef}
+          />
+        </div>
+        <div>
+            <button className="LoginBtn">Login</button>
+          </div>
+          <span className="tosignup">
+            <p>
+              Don't have Account? <Link to="/login">Sign Up</Link>
+            </p>
+          </span>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
